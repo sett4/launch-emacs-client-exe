b9049f72596677ea929dcc885db5825ad7120b6f
sett4 <sett4@jaro68.org>
2008-10-07 04:39:31 +0900

    first commit

---
+++ launch-emacs-client-exe/Launcher.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Management;
+
+namespace jp.jaro68.emacslauncher
+{
+    class Launcher
+    {
+        static void Main(string[] args)
+        {
+
+            if (args.Length == 0)
+            {
+                return;
+            }
+
+            string targetExecutableName = System.Configuration.ConfigurationManager.AppSettings["targetExecutableName"];
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("select * from Win32_Process where ExecutablePath like '%"+targetExecutableName+"'");
+            bool isRunning = false;
+
+            foreach (ManagementObject service in searcher.Get())
+            {
+                isRunning = true;
+            }
+
+            string executable;
+            string executableArgs;
+            if (isRunning == false)
+            {
+                executable = System.Configuration.ConfigurationManager.AppSettings["editor"];
+                if (System.IO.File.Exists(executable) == false)
+                {
+                    executable = AppDomain.CurrentDomain.BaseDirectory + System.Configuration.ConfigurationManager.AppSettings["editorDefault"];
+                }
+                executableArgs = System.Configuration.ConfigurationManager.AppSettings["editorArgs"];
+            }
+            else
+            {
+                executable = System.Configuration.ConfigurationManager.AppSettings["emacsClient"];
+                if (System.IO.File.Exists(executable) == false)
+                {
+                    executable = AppDomain.CurrentDomain.BaseDirectory + System.Configuration.ConfigurationManager.AppSettings["emacsClientDefault"];
+                }
+                executableArgs = System.Configuration.ConfigurationManager.AppSettings["emacsClientArgs"];
+            }
+
+
+            if (System.IO.File.Exists(args[0]) == false)
+            {
+                args = new String[1]{String.Join(" ", args)};
+            }
+
+            foreach (string arg in args)
+            {
+                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo(executable, executableArgs + " \"" + arg + "\"");
+                startInfo.UseShellExecute = false;
+                System.Diagnostics.Process.Start(startInfo);
+            }
+        }
+    }
+}
